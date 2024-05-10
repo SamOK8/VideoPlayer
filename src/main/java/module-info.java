2dddc72ef3cd5b20f8dd45d663c582acdba7c28d
Sam <saminkoh@gmail.com>
2024-05-10 11:30:42 +0200

    open method.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,6 @@ module com.example.player {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.media;
-
     requires org.controlsfx.controls;
     requires java.datatransfer;
 
