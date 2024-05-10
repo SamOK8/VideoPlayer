18b71fb8a7ba534965850c2cb0e4dd11759d268d
Sam <saminkoh@gmail.com>
2024-05-10 11:43:45 +0200

    Fullscreen and speed settings fixed some bugs

--- src/main/java/com/example/player/HelloController.java
+++ src/main/java/com/example/player/HelloController.java
@@ -1,8 +1,17 @@
 package com.example.player;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Slider;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
@@ -11,13 +20,12 @@ import javafx.stage.Stage;
 import javafx.util.Duration;
 
 import java.io.File;
-import java.net.URL;
-import java.util.ResourceBundle;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class HelloController {
-
+    @FXML
+    private ChoiceBox speedBox;
     @FXML
     private Button openButton;
     @FXML
@@ -28,26 +36,39 @@ public class HelloController {
     private Slider progressSlider;
     @FXML
     private Slider volumeSlider;
+    @FXML
+    private HBox Controls;
+    @FXML
+    private VBox scene;
 
     private File file;
     private MediaPlayer mediaPlayer;
     private Media media;
-    private Boolean isPlaying = false;
     private Timer timer;
     private TimerTask task;
+    private double[] speeds = {0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2};
 
+    public void initialize(){
+
+        for (int i = 0; i < speeds.length; i++){
+            speedBox.getItems().add(speeds[i]);
+        }
+        speedBox.setOnAction(this::setSpeed);
+
+    }
     public void playPaue(){
-        if (isPlaying){
-            pause();
-        }else {
-            play();
+        if(mediaPlayer != null){
+            if (MediaPlayer.Status.PLAYING.equals(mediaPlayer.getStatus())){
+                pause();
+            }else {
+                play();
+            }
         }
     }
     public void play(){
         if (mediaPlayer != null){
             beginTimer();
             mediaPlayer.play();
-            isPlaying = true;
             playPauseButton.setText("⏸");
         }
     }
@@ -55,7 +76,13 @@ public class HelloController {
         if (mediaPlayer != null){
             mediaPlayer.pause();
             endTimer();
-            isPlaying = false;
+            playPauseButton.setText("▶");
+        }
+    }
+    public void stop(){
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+            endTimer();
             playPauseButton.setText("▶");
         }
     }
@@ -64,17 +91,15 @@ public class HelloController {
         FileChooser fileChooser = new FileChooser();
         Stage stage = (Stage) openButton.getScene().getWindow();
         file = fileChooser.showOpenDialog(stage);
-        media = new Media(file.toURI().toString());
-        mediaPlayer = new MediaPlayer(media);
-        player.setMediaPlayer(mediaPlayer);
-        isPlaying = true;
-        beginTimer();
-        mediaPlayer.play();
+        //if (file != null) {
+            media = new Media(file.toURI().toString());
+            mediaPlayer = new MediaPlayer(media);
+            player.setMediaPlayer(mediaPlayer);
+            play();
+            volumeSlider.valueProperty().addListener((observable, oldValue, newValue) -> mediaPlayer.setVolume(newValue.doubleValue() / 100));
+            volumeSlider.setValue(100);
 
 
-
-        volumeSlider.valueProperty().addListener((observable, oldValue, newValue) -> mediaPlayer.setVolume(newValue.doubleValue() / 100));
-        volumeSlider.setValue(100);
 
         progressSlider.valueProperty().addListener((observable, oldValue, newValue) -> {
             if (progressSlider.isPressed()) {
@@ -85,10 +110,43 @@ public class HelloController {
                 System.out.println(position);
                 mediaPlayer.stop();
                 mediaPlayer.setStartTime(position);
-                mediaPlayer.play();
+                play();
+
             }
         });
+        Scene scene = playPauseButton.getScene();
+        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent keyEvent) {
+                String key = String.valueOf(keyEvent.getCode());
+                System.out.println(key);
+                if (key.equals("ESCAPE")) {
+                    setScene();
+                }
+            }
+        });
+     //}
+    }
+    public void fullScreen(){
+        if(!((Stage)player.getScene().getWindow()).isFullScreen()){
+            ((Stage)player.getScene().getWindow()).setFullScreen(true);
+            setScene();
+        } else if (((Stage) player.getScene().getWindow()).isFullScreen()) {
+            ((Stage)player.getScene().getWindow()).setFullScreen(false);
+            setScene();
+        }
 
+
+    }
+    private void setScene(){
+        Controls.setPrefWidth(scene.getWidth()-40);
+        player.setFitWidth(scene.getWidth()-89);
+        player.setFitHeight(scene.getHeight()-150);
+    }
+    private void setSpeed(Event event){
+        if (mediaPlayer != null) {
+            mediaPlayer.setRate(Double.parseDouble(String.valueOf(speedBox.getValue())));
+        }
     }
 
     public void beginTimer() {
@@ -97,12 +155,14 @@ public class HelloController {
         task = new TimerTask() {
             @Override
             public void run() {
-                if (mediaPlayer != null) {
+                if (mediaPlayer != null && media != null) {
                     double total = media.getDuration().toSeconds();
                     double current = mediaPlayer.getCurrentTime().toSeconds();
                     if (!progressSlider.isPressed()) {
                         progressSlider.setValue((current / total) * 100);
                     }
+                    mediaPlayer.setOnEndOfMedia(() ->
+                            stop());
                 }
 
             }
